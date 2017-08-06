2d8396fff17f273f39e62c06c01ffb7bc0817d4c
CeceAragorn <igkepsi@gmail.com>
2017-08-06 15:42:13 -0300

    Trying again

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const settings = require('./settings.json');
 
-client.on('ready'.() => {
+client.on(ready => {
   console.log("IT'S ALIVE\nIT'S ALIIIIIIVE");
 });
 
-client.on('message'.message => {
-  if (message.content === "ping") {
-    message.reply("pong!");
-  }
-});
+//client.on(message => {
+//  if (message.content === "ping") {
+//    message.reply("pong!");
+//  }
+//});
 
 client.login(settings.token);
