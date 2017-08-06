b9c4d38b1fe33004028e6c0c58adccd1ae02d441
CeceAragorn <igkepsi@gmail.com>
2017-08-06 18:31:16 -0300

    First working build

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const settings = require('./settings.json');
 
-client.on(ready => {
+client.on('ready', () => {
   console.log("IT'S ALIVE\nIT'S ALIIIIIIVE");
 });
 
-//client.on(message => {
-//  if (message.content === "ping") {
-//    message.reply("pong!");
-//  }
-//});
+var prefix = "!";
+client.on('message', message => {
+  var lowerMessage = message.content;
+  var lowerMessage = lowerMessage.toLowerCase();
+  if (lowerMessage === prefix + "ping") {
+    message.reply("pong!");
+  }
+});
 
 client.login(settings.token);
