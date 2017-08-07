d2db5ac424401353539fa0a6355864dbd315dc37
CeceAragorn <igkepsi@gmail.com>
2017-08-07 12:01:39 -0300

    Less redundancy, more redundancy, TODO.
    Also, checking ping works now. Last night it was outputting negative numbers for some reason. Now it's a normal 1s ping. Also added role and channel IDs to the settings, since we'll be checking on those.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const settings = require('./settings.json');
-const prefix = require('./settings.json').prefix;
+const prefix = settings.prefix;
+const lv = settings.levels; // unused for now
 
 client.on('ready', () => {
-  console.log("IT'S ALIVE\nIT'S ALIIIIIIVE");
+  console.log(`IT LIVES (at ${new Date()})`);
 });
 
 client.on('message', message => {
@@ -12,29 +13,27 @@ client.on('message', message => {
   if (message.author.bot) { return; } // Compatibilidade com outros bots. Se algum bot disser !ping não fará nada
   console.log("Mensagem com prefix vista!");
 
-  var lowerMessage = message.content;
+  var lowerMessage = message.content; // may be redundant
   var lowerMessage = lowerMessage.toLowerCase();
+  var command = lowerMessage.slice(1); // removes prefix since we already know it's there
+
+  // write code to support arguments using string split into an array & check amount of arguments so it works
+  // index 0 will be command, if command expects argument(s) check other indexes
 
   // probably want to rewrite this switch statement it's kind of hard to make arguments in commands using a switch statement
   // but if you don't want to do args in commands keep using this because it's far better code than using the clunky if statements
-  switch (lowerMessage){
-    case prefix + "ping":
+  switch (command){
+    case "ping":
       console.log(`Ping sent at ${Date.now()}`);
-      message.reply("pong!");
+      message.reply(`Pong! I'm at: \`${Date.now() - message.createdTimestamp}ms\``);
       break;
-    case prefix + "unix":
+    case "unix":
       console.log(`Unix sent at ${Date.now()}`);
       message.channel.send(`Ponged: \`${Date.now()}\`ms!`);
       break;
-    case prefix + "date":
+    case "date":
       console.log(`Date sent at ${Date.now()}`);
       message.channel.send(`Current time and date is \`${new Date()}\`.`);
-      break;
-    case prefix + "prefix":
-      console.log(`Prefix sent at ${Date.now()}`);
-      message.channel.send(`Current prefix is \`${prefix}\``);
-  }
-
 });
 
 client.login(settings.token);
