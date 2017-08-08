40465a82f98c06d7a20f776cd5ec988d23d377a4
CeceAragorn <igkepsi@gmail.com>
2017-08-07 23:57:15 -0300

    gj on arguments. removed redundancies and added some edge cases

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const settings = require('./settings.json');
 const prefix = settings.prefix;
-const lv = settings.levels; // unused for now
+const lv = settings.levels;
+const pw = lv.passwords;
+const role = lv.roles;
+const room = lv.rooms;
 
 client.on('ready', () => {
   console.log(`IT LIVES (at ${new Date()})`);
@@ -14,7 +17,7 @@ client.on('message', message => {
   console.log("Mensagem com prefix vista!");
 
   var command = message.content.slice(1); // removes prefix since we already know it's there
-  var args = command.split(" "); // splits the command in  array of arguments
+  var args = command.split(" "); // splits the command & the arguments into an array. may want to rewrite for support of arguments with spaces ala !setthings "This is thing1." "This is thing2!" Thing3 Thing4 Thing5
   args[0]=args[0].toLowerCase();
 
   // probably want to rewrite this switch statement it's kind of hard to make arguments in commands using a switch statement
@@ -35,14 +38,24 @@ client.on('message', message => {
       message.channel.send(`Current time and date is \`${new Date()}\`.`);
       break;
     case "password":
-      if(args[1] == lv.passwords.l0l1) {
-        message.author.send(`You have passed to the next level.`);
-        message.guild.channels.find("id", lv.rooms.dev).send(`${message.author.username} just passed his level by sending \`${message.content}\`.`);
-        message.delete();
-        message.member.addRole(lv.roles.l1);
-        message.member.removeRole(lv.roles.l0);
-        break;
-     } else { break; }
+      if(!args[1]) {
+        message.reply("You have to include the password!");
+        return;
+      }
+      console.log("I see "+args[1]+" as the password. Everything else is ignored.")
+      //if(args[1] == pw.l0l1) {
+      switch(args[1]) {
+        case pw.l0l1:
+          message.author.send(`You have passed to the next level.`);
+          message.guild.channels.find("id", room.dev).send(`${message.author.username} just passed his level by sending \`${message.content}\`.`);
+          message.delete();
+          message.member.addRole(role.l1);
+          message.member.removeRole(role.l0);
+          break;
+        default:
+          message.reply("I'm sorry, but you got it wrong. Try again.");
+          message.delete(); //remove this if you want but i prefer not doing this incase they get a typo off the answer and it makes it obvious
+     }
   }
 });
 
