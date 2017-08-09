6e3185b02218c51d06c482f573bb99b3b2eb77d1
lsfonseca1 <luisfonseca720@gmail.com>
2017-08-09 17:07:46 +0200

    Added a new password handler

--- app.js
+++ app.js
@@ -8,21 +8,43 @@ const role = lv.roles;
 const room = lv.rooms;
 const strpattern = /[^\s"']+|"([^"]*)"|'([^']*)'/g;
 
+function getPw(password,message) {
+  for (var key in pw) {
+    
+    if (pw.hasOwnProperty(key)) {
+        
+      if (password == pw[key] && message.member.roles.has(lv.roles[key.substring(0,2)])) {
+        
+        console.log(key + " --> " + pw[key]);
+        
+        message.author.send(`You have passed to the next level.`);
+        message.guild.channels.find("id", room.dev).send(`${message.author.username} just passed from level ${key.substring(1,2)} to ${key.substring(3,4)} level by sending \`${message.content}\`.`);
+        console.log(`${message.author.username} just passed from level ${key.substring(1,2)} to ${key.substring(3,4)} level by sending \`${message.content}\`.`)
+        message.delete();
+        message.member.addRole(lv.roles[key.substring(2,4)]);
+        message.member.removeRole(lv.roles[key.substring(0,2)]);
+      } else {
+        message.author.send(`\`${password}\` is wrong, please try again`);
+        message.delete();
+      }
+    }
+  }
+}
+
+
 client.on('ready', () => {
   console.log(`IT LIVES (at ${new Date()})`);
 });
 
 client.on('message', message => {
-  if (!message.content.startsWith(prefix)) { return; } // Caso a mensagem não é um comando, ignore. Provávelmente retiraremos isso para a senha funcionar direito.
+  if (!message.content.startsWith(prefix)) { return; } // Caso a mensagem não é um comando, ignore.
   if (message.author.bot) { return; } // Compatibilidade com outros bots. Se algum bot disser !ping não fará nada
   console.log("A wild prefix has appeared!");
 
   sorted = message.content.match(strpattern);
   sorted[0] = sorted[0].toLowerCase().slice(1); //removes the prefix
   console.log(sorted);
-
-
-
+  
   //note to self if (message.member.roles.has(lv.roles.dev))
   switch (sorted[0]){
     case "ping":
@@ -37,24 +59,18 @@ client.on('message', message => {
       console.log(`Date sent at ${Date.now()}`);
       message.channel.send(`Current time and date is \`${new Date()}\`.`);
       break;
+    case "donate":
+      console.log('Donate sent at ${Date.now()}');
+      message.channel.send('Thanks for donating!\nBitcoin: http://imgur.com/DW8BZDc');
+      break;
     case "password":
+
       if(!sorted[1]) {
         message.reply("You have to include the password!");
         return;
       }
       console.log("I see "+sorted[1]+" as the password. Everything else is ignored.")
-      switch(sorted[1]) {
-        case pw.l0l1:
-          message.author.send(`You have passed to the next level.`);
-          message.guild.channels.find("id", room.dev).send(`${message.author.username} just passed his level by sending \`${message.content}\`.`);
-          message.delete();
-          message.member.addRole(role.l1);
-          message.member.removeRole(role.l0);
-          break;
-        default:
-          message.reply("I'm sorry, but you got it wrong. Try again.");
-          message.delete(); //remove this if you want but i prefer not doing this incase they get a typo off the answer and it makes it obvious
-     }
+      getPw(sorted[1],message);
   }
 });
 
